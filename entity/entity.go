52bb9503798d4e84cea66d38b94acaf5ab267760
DemoonLXW <924469123@qq.com>
2023-04-26 19:52:10 +0800

    Add a Package: Entity Add a entity: Permission

---
+++ entity/entity.go
@@ -0,0 +1,12 @@
+package entity
+
+import "time"
+
+type Permission struct {
+	ID           uint16    `gorm:"primaryKey;column:id;"`
+	Action       string    `gorm:"column:action;"`
+	Description  string    `gorm:"column:description;"`
+	CreateTime   time.Time `gorm:"column:created_time;"`
+	DeletedTime  time.Time `gorm:"column:deleted_time;"`
+	ModifiedTime time.Time `gorm:"column:modified_time;"`
+}
