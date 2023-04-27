7905d74dfd0360b56a813d11363168cdf294494b
DemoonLXW <924469123@qq.com>
2023-04-27 18:18:16 +0800

    Add a Function: UpdatePermission TestPass

--- dao/dao_test.go
+++ dao/dao_test.go
@@ -24,3 +24,20 @@ func TestCreatePermission(t *testing.T) {
 	assert.Nil(t, err)
 
 }
+
+func TestUpdatePermission(t *testing.T) {
+	os.Setenv("DB_CONFIG", "../database.config.json")
+	dao := new(PermissionDao)
+	db, err := injection.ProvideDataBase()
+	assert.Nil(t, err)
+	dao.DB = db
+
+	permission := entity.Permission{
+		ID:          1,
+		Action:      "test update action",
+		Description: "test update description",
+	}
+	err = dao.UpdatePermission(&permission)
+	assert.Nil(t, err)
+
+}
--- dao/permission.dao.go
+++ dao/permission.dao.go
@@ -29,7 +29,6 @@ func (dao *PermissionDao) CreatePermission(permission *entity.Permission) error 
 			tx.Rollback()
 		}
 	}()
-
 	if err := tx.Error; err != nil {
 		return err
 	}
@@ -38,9 +37,36 @@ func (dao *PermissionDao) CreatePermission(permission *entity.Permission) error 
 	permission.CreateTime = time.Now()
 	if err := tx.Omit("deleted_time", "modified_time").Create(permission).Error; err != nil {
 		tx.Rollback()
-		return err
+		return errors.Join(errors.New("delete fail"), err)
 	}
 
 	return tx.Commit().Error
 
+}
+
+func (dao *PermissionDao) UpdatePermission(permission *entity.Permission) error {
+	var check entity.Permission
+	if err := dao.DB.Where("id = ?", permission.ID).First(&check).Error; err != nil {
+		return errors.Join(errors.New("permission can not be found"), err)
+	}
+	if !check.DeletedTime.IsZero() {
+		return errors.New("permission is deleted")
+	}
+
+	tx := dao.DB.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+	if err := tx.Error; err != nil {
+		return err
+	}
+	permission.ModifiedTime = time.Now()
+	if err := tx.Model(permission).Omit("id", "created_time", "deleted_time").Updates(permission).Error; err != nil {
+		tx.Rollback()
+		return errors.Join(errors.New("update permission fail"), err)
+	}
+
+	return tx.Commit().Error
 }
