96347c779628f08322737c9343175a90db254e4e
DemoonLXW <924469123@qq.com>
2023-06-13 22:05:21 +0800

    Add a Function: CreateUser Test Pass

--- service/management.service.go
+++ service/management.service.go
@@ -9,6 +9,7 @@ import (
 	"github.com/DemoonLXW/up_learning/database/ent"
 	"github.com/DemoonLXW/up_learning/database/ent/permission"
 	"github.com/DemoonLXW/up_learning/database/ent/role"
+	"github.com/DemoonLXW/up_learning/database/ent/user"
 )
 
 type ManagementService struct {
@@ -192,7 +193,7 @@ func (serv *ManagementService) CreateRole(toCreates []*ent.Role) error {
 
 	checkRepeatName, err := serv.DB.Role.Query().Where(role.NameIn(names...)).Exist(ctx)
 	if err != nil {
-		return fmt.Errorf("create roles check repeat name failed: %w", err)
+		return fmt.Errorf("create roles check repeat role failed: %w", err)
 	}
 	if checkRepeatName {
 		return fmt.Errorf("repeat role")
@@ -325,4 +326,49 @@ func (serv *ManagementService) DeleteRole(toDeleteIDs []uint8) error {
 	}
 
 	return nil
+}
+
+func (serv *ManagementService) CreateUser(toCreates []*ent.User) error {
+	ctx := context.Background()
+
+	num, err := serv.DB.User.Query().Aggregate(ent.Count()).Int(ctx)
+	if err != nil {
+		return fmt.Errorf("create users count failed: %w", err)
+	}
+
+	accounts := make([]string, len(toCreates))
+	bulk := make([]*ent.UserCreate, len(toCreates))
+	for i, v := range toCreates {
+		accounts[i] = *v.Account
+		bulk[i] = serv.DB.User.Create().
+			SetID(uint32(num + i + 1)).
+			SetAccount(*v.Account).
+			SetPassword(*v.Password)
+	}
+
+	checkRepeatAccount, err := serv.DB.User.Query().Where(user.AccountIn(accounts...)).Exist(ctx)
+	if err != nil {
+		return fmt.Errorf("create users check repeat user failed: %w", err)
+	}
+	if checkRepeatAccount {
+		return fmt.Errorf("repeat user")
+	}
+
+	tx, err := serv.DB.Tx(ctx)
+	if err != nil {
+		return fmt.Errorf("create user start a transaction failed: %w", err)
+	}
+
+	err = tx.User.CreateBulk(bulk...).Exec(ctx)
+	if err != nil {
+		return rollback(tx, "create users", err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return fmt.Errorf("create users transaction commit failed: %w", err)
+	}
+
+	return nil
+
 }
