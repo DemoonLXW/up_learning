572da3980d009bac713a73462af8e40b14764554
DemoonLXW <924469123@qq.com>
2023-06-14 23:36:59 +0800

    Update a Service: Management

--- service/management.service.go
+++ service/management.service.go
@@ -9,18 +9,10 @@ import (
 	"github.com/DemoonLXW/up_learning/database/ent"
 	"github.com/DemoonLXW/up_learning/database/ent/permission"
 	"github.com/DemoonLXW/up_learning/database/ent/role"
-	"github.com/DemoonLXW/up_learning/database/ent/user"
 )
 
 type ManagementService struct {
 	DB *ent.Client
-}
-
-func rollback(tx *ent.Tx, description string, err error) error {
-	if rerr := tx.Rollback(); rerr != nil {
-		return fmt.Errorf("%s rollback failed %w: %v", description, err, rerr)
-	}
-	return fmt.Errorf("%s failed: %w", description, err)
 }
 
 func (serv *ManagementService) CreatePermission(toCreates []*ent.Permission) error {
@@ -326,49 +318,4 @@ func (serv *ManagementService) DeleteRole(toDeleteIDs []uint8) error {
 	}
 
 	return nil
-}
-
-func (serv *ManagementService) CreateUser(toCreates []*ent.User) error {
-	ctx := context.Background()
-
-	num, err := serv.DB.User.Query().Aggregate(ent.Count()).Int(ctx)
-	if err != nil {
-		return fmt.Errorf("create users count failed: %w", err)
-	}
-
-	accounts := make([]string, len(toCreates))
-	bulk := make([]*ent.UserCreate, len(toCreates))
-	for i, v := range toCreates {
-		accounts[i] = *v.Account
-		bulk[i] = serv.DB.User.Create().
-			SetID(uint32(num + i + 1)).
-			SetAccount(*v.Account).
-			SetPassword(*v.Password)
-	}
-
-	checkRepeatAccount, err := serv.DB.User.Query().Where(user.AccountIn(accounts...)).Exist(ctx)
-	if err != nil {
-		return fmt.Errorf("create users check repeat user failed: %w", err)
-	}
-	if checkRepeatAccount {
-		return fmt.Errorf("repeat user")
-	}
-
-	tx, err := serv.DB.Tx(ctx)
-	if err != nil {
-		return fmt.Errorf("create user start a transaction failed: %w", err)
-	}
-
-	err = tx.User.CreateBulk(bulk...).Exec(ctx)
-	if err != nil {
-		return rollback(tx, "create users", err)
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return fmt.Errorf("create users transaction commit failed: %w", err)
-	}
-
-	return nil
-
 }
--- service/management.service_test.go
+++ service/management.service_test.go
@@ -156,28 +156,3 @@ func TestDeleteRole(t *testing.T) {
 	assert.Nil(t, err)
 
 }
-
-func TestCreateUser(t *testing.T) {
-	os.Setenv("DB_CONFIG", "../database.config.json")
-	serv := new(ManagementService)
-	db, err := injection.ProvideDataBase()
-	assert.Nil(t, err)
-	serv.DB = db
-
-	account1 := "username3"
-	password1 := "password3"
-	user1 := ent.User{
-		Account:  &account1,
-		Password: &password1,
-	}
-
-	account2 := "account3"
-	password2 := "password2"
-	user2 := ent.User{
-		Account:  &account2,
-		Password: &password2,
-	}
-	err = serv.CreateUser([]*ent.User{&user2, &user1})
-	assert.Nil(t, err)
-
-}
