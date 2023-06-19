62156c3d08f8846053c8d005be50461d8f8effd9
DemoonLXW <924469123@qq.com>
2023-06-19 15:38:29 +0800

    Update UserService: Add Login Test Pass

--- service/user.service.go
+++ service/user.service.go
@@ -1,7 +1,85 @@
 package service
 
-import "github.com/DemoonLXW/up_learning/database/ent"
+import (
+	"context"
+	"crypto/md5"
+	"fmt"
+	"time"
+
+	"github.com/DemoonLXW/up_learning/database/ent"
+	"github.com/DemoonLXW/up_learning/database/ent/user"
+	"github.com/redis/go-redis/v9"
+)
 
 type UserService struct {
-	DB *ent.Client
+	DB    *ent.Client
+	Redis *redis.Client
+}
+
+func (serv *UserService) Login(account, password string) (string, error) {
+	ctx := context.Background()
+
+	u, err := serv.DB.User.Query().Where(
+		user.And(
+			user.Or(
+				user.Account(account),
+				user.Email(account),
+				user.Phone(account),
+			),
+			user.Password(password),
+			user.DeletedTimeEQ(time.Date(1999, time.November, 11, 0, 0, 0, 0, time.Local)),
+		),
+	).First(ctx)
+	if err != nil {
+		return "", fmt.Errorf("user login check credential failed: %w", err)
+	}
+
+	expire_date := time.Now().In(time.Local)
+	expire_date_byte, err := expire_date.MarshalText()
+	if err != nil {
+		return "", fmt.Errorf("user login convert time to string failed: %w", err)
+	}
+	key := fmt.Sprintf("user:%d", u.ID)
+	cookie := fmt.Sprintf("%x", md5.Sum([]byte(key+expire_date.String())))
+	user_cookies, err := serv.Redis.HGetAll(ctx, key).Result()
+	switch {
+	case err == redis.Nil, err == nil && len(user_cookies) < 5:
+		{
+			_, err = serv.Redis.HSet(ctx, key, cookie, string(expire_date_byte)).Result()
+			if err != nil {
+				return "", fmt.Errorf("user login set cookie failed: %w", err)
+			}
+		}
+	case err != nil:
+		{
+			return "", fmt.Errorf("user login get cookies failed: %w", err)
+		}
+	case len(user_cookies) >= 5:
+		{
+			to_delete_key := ""
+			to_delete_time := time.Now()
+			for k, v := range user_cookies {
+				expire_time, err := time.Parse(time.RFC3339, v)
+				if err != nil {
+					return "", fmt.Errorf("user login parse string to time failed: %w", err)
+				}
+				if expire_time.Before(to_delete_time) {
+					to_delete_key = k
+					to_delete_time = expire_time
+				}
+			}
+
+			_, err := serv.Redis.HDel(ctx, key, to_delete_key).Result()
+			if err != nil {
+				return "", fmt.Errorf("user login delete cookie failed: %w", err)
+			}
+
+			_, err = serv.Redis.HSet(ctx, key, cookie, string(expire_date_byte)).Result()
+			if err != nil {
+				return "", fmt.Errorf("user login set cookie failed: %w", err)
+			}
+		}
+	}
+
+	return cookie, nil
 }
--- service/user.service_test.go
+++ service/user.service_test.go
@@ -1,1 +1,31 @@
 package service
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/DemoonLXW/up_learning/injection"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLogin(t *testing.T) {
+	os.Setenv("DB_CONFIG", "../database.config.json")
+	serv := new(UserService)
+	db, err := injection.ProvideDataBase()
+	assert.Nil(t, err)
+	serv.DB = db
+	rd, err := injection.ProvideRedis()
+	assert.Nil(t, err)
+	serv.Redis = rd
+
+	// for i := 0; i < 2; i++ {
+	// 	cookie, err := serv.Login("username1", "password1")
+	// 	assert.Nil(t, err)
+	// 	fmt.Println(cookie)
+	// 	time.Sleep(3 * time.Second)
+	// }
+	cookie, err := serv.Login("account3", "password2")
+	assert.Nil(t, err)
+	fmt.Println(cookie)
+}
