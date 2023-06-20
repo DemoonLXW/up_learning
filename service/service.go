bcbd8521cf4178b7f6c3f1ee5fbb7b91fee2eeb5
DemoonLXW <924469123@qq.com>
2023-06-20 21:34:45 +0800

    Update Package: Service For Injection

--- service/service.go
+++ service/service.go
@@ -4,7 +4,19 @@ import (
 	"fmt"
 
 	"github.com/DemoonLXW/up_learning/database/ent"
+	"github.com/google/wire"
 )
+
+var ServiceProvider = wire.NewSet(
+	wire.Struct(new(Service), "*"),
+	wire.Struct(new(ManagementService), "*"),
+	wire.Struct(new(UserService), "*"),
+)
+
+type Service struct {
+	Management *ManagementService
+	User       *UserService
+}
 
 func rollback(tx *ent.Tx, description string, err error) error {
 	if rerr := tx.Rollback(); rerr != nil {
